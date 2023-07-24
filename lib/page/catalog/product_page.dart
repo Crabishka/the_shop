9a95c93414c0132cd64ab42f63f8e20ba1ceef33
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-07-24 13:10:47 +0300

    feature/start product page

---
+++ lib/page/catalog/product_page.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class ProductPage extends StatelessWidget {
+  const ProductPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+
+    );
+  }
+}
